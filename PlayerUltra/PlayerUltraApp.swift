1f6ed0567737f26287543acc65322b05885ff83f
NovikovValeriy <novikov.valera2005@gmail.com>
2026-09-10 11:32:44 +0200

    Initial Commit

---
+++ PlayerUltra/PlayerUltraApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayerUltraApp.swift
+//  PlayerUltra
+//
+//  Created by user on 10/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct PlayerUltraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
